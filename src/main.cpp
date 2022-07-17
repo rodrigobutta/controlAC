28421ca1fd02cb81faa2313b6ed280f510a0272f
rodrigobutta <rbutta@gmail.com>
2022-07-17 00:39:28 -0300

    ir to mqtt state

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <string>
+#include <IRrecv.h>
+#include <IRac.h>
+#include <IRtext.h>
+#include <IRutils.h>
 #include "settings.h"
 #include "wifiConnection.h"
 
@@ -21,8 +25,100 @@ long refreshPrevMillis = 0;
 int statusLedState = LOW;             // statusLedState used to set the LED
 unsigned long statusLedPrevMillis = 0;        // will store last time LED was updated
 
-// int temp = 0;             // statusLedState used to set the LED
 
+
+
+
+
+// #############################
+// ########## IR READ ##########
+// #############################
+
+const uint16_t kRecvPin = 15; 
+
+
+// As this program is a special purpose capture/decoder, let us use a larger
+// than normal buffer so we can handle Air Conditioner remote codes.
+const uint16_t kCaptureBufferSize = 1024;
+
+
+// kTimeout is the Nr. of milli-Seconds of no-more-data before we consider a
+// message ended.
+// This parameter is an interesting trade-off. The longer the timeout, the more
+// complex a message it can capture. e.g. Some device protocols will send
+// multiple message packets in quick succession, like Air Conditioner remotes.
+// Air Coniditioner protocols often have a considerable gap (20-40+ms) between
+// packets.
+// The downside of a large timeout value is a lot of less complex protocols
+// send multiple messages when the remote's button is held down. The gap between
+// them is often also around 20+ms. This can result in the raw data be 2-3+
+// times larger than needed as it has captured 2-3+ messages in a single
+// capture. Setting a low timeout value can resolve this.
+// So, choosing the best kTimeout value for your use particular case is
+// quite nuanced. Good luck and happy hunting.
+// NOTE: Don't exceed kMaxTimeoutMs. Typically 130ms.
+#if DECODE_AC
+// Some A/C units have gaps in their protocols of ~40ms. e.g. Kelvinator
+// A value this large may swallow repeats of some protocols
+const uint8_t kTimeout = 50;
+#else   // DECODE_AC
+// Suits most messages, while not swallowing many repeats.
+const uint8_t kTimeout = 15;
+#endif  // DECODE_AC
+// Alternatives:
+// const uint8_t kTimeout = 90;
+// Suits messages with big gaps like XMP-1 & some aircon units, but can
+// accidentally swallow repeated messages in the rawData[] output.
+//
+// const uint8_t kTimeout = kMaxTimeoutMs;
+// This will set it to our currently allowed maximum.
+// Values this high are problematic because it is roughly the typical boundary
+// where most messages repeat.
+// e.g. It will stop decoding a message and start sending it to serial at
+//      precisely the time when the next message is likely to be transmitted,
+//      and may miss it.
+
+// Set the smallest sized "UNKNOWN" message packets we actually care about.
+// This value helps reduce the false-positive detection rate of IR background
+// noise as real messages. The chances of background IR noise getting detected
+// as a message increases with the length of the kTimeout value. (See above)
+// The downside of setting this message too large is you can miss some valid
+// short messages for protocols that this library doesn't yet decode.
+//
+// Set higher if you get lots of random short UNKNOWN messages when nothing
+// should be sending a message.
+// Set lower if you are sure your setup is working, but it doesn't see messages
+// from your device. (e.g. Other IR remotes work.)
+// NOTE: Set this value very high to effectively turn off UNKNOWN detection.
+const uint16_t kMinUnknownSize = 12;
+
+// How much percentage lee way do we give to incoming signals in order to match
+// it?
+// e.g. +/- 25% (default) to an expected value of 500 would mean matching a
+//      value between 375 & 625 inclusive.
+// Note: Default is 25(%). Going to a value >= 50(%) will cause some protocols
+//       to no longer match correctly. In normal situations you probably do not
+//       need to adjust this value. Typically that's when the library detects
+//       your remote's message some of the time, but not all of the time.
+const uint8_t kTolerancePercentage = kTolerance;  // kTolerance is normally 25%
+
+// Legacy (No longer supported!)
+//
+// Change to `true` if you miss/need the old "Raw Timing[]" display.
+#define LEGACY_TIMING_INFO false
+// ==================== end of TUNEABLE PARAMETERS ====================
+
+// Use turn on the save buffer feature for more complete capture coverage.
+IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
+decode_results results;  // Somewhere to store the results
+
+
+
+
+
+// #############################
+// ########## IR SEND ##########
+// #############################
 
 // const uint8_t kLgAcFanLowest = 0;  // 0b0000
 // const uint8_t kLgAcFanLow = 1;     // 0b0001
@@ -113,6 +209,7 @@ char deviceName[] = "AC Remote Control";
 
 const std::string tempKey = "temp";
 const std::string modeKey = "mode";
+const std::string fanKey = "fan";
 
 
 void mqttSend(char* topic, char* message) {
@@ -211,6 +308,16 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
         }
       }
 
+      if(payloadJson.containsKey(fanKey)) {
+        const String fanValue = payloadJson[fanKey];
+        if (fanValue) {
+          if (fanValue.length() > 1) {
+            Serial.println(">> Updating FAN...");
+            state[fanKey] = fanValue;
+          }
+        }
+      }
+
           
     // al final de todo el proceso, porque si va en el medio corta el resto
     broadcastState();
@@ -265,29 +372,34 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Hello Rodrigo Butta");
 
-  
   wifiSetup();
   mqttSetup();
   
   ac.begin();
+
+
+#if DECODE_HASH
+  // Ignore messages with less than minimum on or off pulses.
+  irrecv.setUnknownThreshold(kMinUnknownSize);
+#endif  // DECODE_HASH
+  irrecv.setTolerance(kTolerancePercentage);  // Override the default tolerance.
+  irrecv.enableIRIn();  // Start the receiver
 }
 
 
 void refreshLoop() {
     // Convert the value to a char array
-    char tempString[8];
-    dtostrf(state[tempKey], 1, 2, tempString);
-    Serial.print("Temperature: ");
-    Serial.print(tempString);
-    Serial.println();
+    // char tempString[8];
+    // dtostrf(state[tempKey], 1, 2, tempString);
+    // Serial.print("Temperature: ");
+    // Serial.print(tempString);
+    // Serial.println();
 
-    char modeString[8];
-    dtostrf(state[modeKey], 1, 2, modeString);
-    Serial.print("Mode: ");
-    Serial.print(modeString);
-    Serial.println();
-
-
+    // char modeString[8];
+    // dtostrf(state[modeKey], 1, 2, modeString);
+    // Serial.print("Mode: ");
+    // Serial.print(modeString);
+    // Serial.println();
 }
 
 
@@ -303,8 +415,120 @@ void statusLedLoop() {
 
 }
 
+
+void irReceiverLoop() {
+  if (irrecv.decode(&results)) {   
+    stdAc::state_t decodedIr, p; 
+    IRAcUtils::decodeToState(&results, &decodedIr, &p);
+
+    Serial.print("Power: ");
+    String powerStr;
+    switch (decodedIr.power) {
+      case true:      powerStr = "on"; break;
+      case false:     powerStr = "off"; break;
+    }
+    Serial.println(powerStr);
+
+    Serial.print("Mode: ");
+
+
+ 
+    String modeStr;
+    switch (decodedIr.mode) {
+      case stdAc::opmode_t::kOff:  modeStr = "off"; break;
+      case stdAc::opmode_t::kAuto: modeStr = "auto"; break;
+      case stdAc::opmode_t::kCool: modeStr = "cool"; break;
+      case stdAc::opmode_t::kHeat: modeStr = "heat"; break;
+      case stdAc::opmode_t::kDry:  modeStr = "dry"; break;
+      case stdAc::opmode_t::kFan:  modeStr = "fan_only"; break;
+      default:                     modeStr = "kUnknownStr";
+    }
+    if(decodedIr.power == false) {
+      modeStr = "off";
+    }
+    Serial.println(modeStr);
+    
+    Serial.print("Temp: ");
+    Serial.println(decodedIr.degrees);
+
+    Serial.print("Fan: ");
+    String fanspeedStr;
+    switch (decodedIr.fanspeed) {
+      case stdAc::fanspeed_t::kAuto:   fanspeedStr = "auto"; break;
+      case stdAc::fanspeed_t::kMax:    fanspeedStr = "max"; break;
+      case stdAc::fanspeed_t::kHigh:   fanspeedStr = "high"; break;
+      case stdAc::fanspeed_t::kMedium: fanspeedStr = "med"; break;
+      case stdAc::fanspeed_t::kLow:    fanspeedStr = "low"; break;
+      case stdAc::fanspeed_t::kMin:    fanspeedStr = "min"; break;
+      default:                         fanspeedStr = "unknown";
+    }
+    Serial.println(fanspeedStr);
+
+    state[tempKey] = decodedIr.degrees;
+    state[modeKey] = modeStr;
+    state[fanKey] = fanspeedStr;
+
+    // Serial.print("SwingV: ");
+    // String swingvStr;
+    // switch (decodedIr.swingv) {
+    //   case stdAc::swingv_t::kOff:     swingvStr = "kOffStr"; break;
+    //   case stdAc::swingv_t::kAuto:    swingvStr = "kAutoStr"; break;
+    //   case stdAc::swingv_t::kHighest: swingvStr = "kHighestStr"; break;
+    //   case stdAc::swingv_t::kHigh:    swingvStr = "kHighStr"; break;
+    //   case stdAc::swingv_t::kMiddle:  swingvStr = "kMiddleStr"; break;
+    //   case stdAc::swingv_t::kLow:     swingvStr = "kLowStr"; break;
+    //   case stdAc::swingv_t::kLowest:  swingvStr = "kLowestStr"; break;
+    //   default:                        swingvStr = "kUnknownStr"; break;
+    // }
+    // Serial.println(swingvStr);
+
+    // Serial.print("SwingH: ");
+    // String swinghStr;
+    // switch (decodedIr.swingh) {
+    //   case stdAc::swingh_t::kOff:      swinghStr = "kOffStr"; break;
+    //   case stdAc::swingh_t::kAuto:     swinghStr = "kAutoStr"; break;
+    //   case stdAc::swingh_t::kLeftMax:  swinghStr = "kLeftMaxStr"; break;
+    //   case stdAc::swingh_t::kLeft:     swinghStr = "kLeftStr"; break;
+    //   case stdAc::swingh_t::kMiddle:   swinghStr = "kMiddleStr"; break;
+    //   case stdAc::swingh_t::kRight:    swinghStr = "kRightStr"; break;
+    //   case stdAc::swingh_t::kRightMax: swinghStr = "kRightMaxStr"; break;
+    //   case stdAc::swingh_t::kWide:     swinghStr = "kWideStr"; break;
+    //   default:                         swinghStr = "kUnknownStr"; break;
+    // }
+    // Serial.println(swinghStr);
+
+    //     struct state_t {
+    //   decode_type_t protocol = decode_type_t::UNKNOWN;
+    //   int16_t model = -1;  // `-1` means unused.
+    //   bool power = false;
+    //   stdAc::opmode_t mode = stdAc::opmode_t::kOff;
+    //   float degrees = 25;
+    //   bool celsius = true;
+    //   stdAc::fanspeed_t fanspeed = stdAc::fanspeed_t::kAuto;
+    //   stdAc::swingv_t swingv = stdAc::swingv_t::kOff;
+    //   stdAc::swingh_t swingh = stdAc::swingh_t::kOff;
+    //   bool quiet = false;
+    //   bool turbo = false;
+    //   bool econo = false;
+    //   bool light = false;
+    //   bool filter = false;
+    //   bool clean = false;
+    //   bool beep = false;
+    //   int16_t sleep = -1;  // `-1` means off.
+    //   int16_t clock = -1;  // `-1` means not set.
+    // };
+
+    broadcastState();
+
+    yield();  // Feed the WDT as the text output can take a while to print.
+    Serial.println();    // Blank line between entries
+    yield();             // Feed the WDT (again)
+  }
+}
+
 void loop() {
   mqttLoop();
+  irReceiverLoop();
 
   unsigned long currentMillis = millis();
 
